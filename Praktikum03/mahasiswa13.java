c95724f0cf77a79ec7444a2835995c61ead01674
JVK50 <jidanali32@gmail.com>
2026-02-24 10:36:03 +0700

    modifikasi

--- Praktikum03/mahasiswa13.java
+++ Praktikum03/mahasiswa13.java
@@ -6,5 +6,11 @@ public class mahasiswa13 {
     public String kelas;
     public float ipk;
 
-    
+    public void cetakInfo() {
+        System.out.println("NIM   : " + this.nim);
+        System.out.println("Nama  : " + this.nama);
+        System.out.println("Kelas : " + this.kelas);
+        System.out.println("IPK   : " + this.ipk);
+        System.out.println("--------------------------------------");
+    }
 }
