f99dffb3b6f056d79e0060417d3ddba1c55b32ac
JVK50 <jidanali32@gmail.com>
2026-02-24 09:54:17 +0700

    Praktikum 3.2

--- Praktikum03/mahasiswaDemo13.java
+++ Praktikum03/mahasiswaDemo13.java
@@ -1,8 +1,36 @@
 package smt2.jobsheet3.Praktikum03;
+// import java.util.Scanner;
 
 public class mahasiswaDemo13 {
     public static void main(String[] args) {
+        // Scanner jvk = new Scanner(System.in);
         mahasiswa13[] arrayOfMahasiswa = new mahasiswa13[3];
+        // String dummy;
+
+        // for (int i = 0; i < 3; i++) {
+        //     arrayOfMahasiswa[i] = new mahasiswa13();
+
+        //     System.out.println("Masukkan data mahasiswa ke-" + (i + 1));
+        //     System.out.print("NIM  : ");
+        //     arrayOfMahasiswa[i].nim = jvk.nextLine();
+        //     System.out.print("Nama : ");
+        //     arrayOfMahasiswa[i].nama = jvk.nextLine();
+        //     System.out.print("Kelas: ");
+        //     arrayOfMahasiswa[i].kelas = jvk.nextLine();
+        //     System.out.print("IPK  : ");
+        //     dummy = jvk.nextLine();
+        //     arrayOfMahasiswa[i].ipk = Float.parseFloat(dummy);
+        //     System.out.println("--------------------------------------");
+        // }
+
+        // for (int i = 0; i < 3; i++) {
+        //     System.out.println("Data mahasiswa ke-" +(i + 1));
+        //     System.out.println("NIM   : " + arrayOfMahasiswa[i].nim);
+        //     System.out.println("Nama  : " + arrayOfMahasiswa[i].nama);
+        //     System.out.println("Kelas : " + arrayOfMahasiswa[i].kelas);
+        //     System.out.println("IPK   : " + arrayOfMahasiswa[i].ipk);
+        //     System.out.println("--------------------------------------");
+        // }
 
         arrayOfMahasiswa[0] = new mahasiswa13();
         arrayOfMahasiswa[0].nim   = "244107060033";
